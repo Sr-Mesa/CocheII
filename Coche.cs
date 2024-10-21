121d8a97c804832e4e16369abc22e01f0dc7fcfa
SrMesa1 <morenomesa12@gmai.com>
2024-10-21 17:44:40 -0500

    Agregar archivos de proyecto.

---
+++ Coche.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CocheII
+{
+    internal class Coche
+    {
+        //Atributos
+        string marca;
+        string modelo;
+        int velocidad;
+
+        public Coche(string marca, string modelo, int velocidad)
+        {
+            this.marca = marca;
+            this.modelo = modelo;
+            this.velocidad = velocidad;
+        }
+
+        //Metodos
+        public int Acelerar(int cantidad)
+        {
+            this.velocidad += cantidad;
+            return this.velocidad;
+        }
+
+        public int Frenar(int cantidad)
+        {
+            if (cantidad <= this.velocidad)
+            {
+                this.velocidad -= cantidad;
+                return this.velocidad;
+            }
+            else
+            {
+
+                this.velocidad=0;
+                
+               
+            }
+            return this.velocidad;
+        }
+
+        public string MostrarInfo()
+        {
+            return "Marca: "+ this.marca +"\nModelo: " + this.modelo +"\nVelocidad: " + this.velocidad+"km/h";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace CocheII
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string marca, modelo;
+            int opcion=0, velocidad = 0, cantidad=0;
+
+            Console.WriteLine("Cual es la marca?");
+            marca= Console.ReadLine();
+
+            Console.WriteLine("Cual es el modelo?");
+            modelo = Console.ReadLine();
+
+            Coche coche = new Coche(marca, modelo,velocidad);
+
+            do
+            {
+
+                Console.WriteLine("----MENU----");
+                Console.WriteLine("1.Acelerar coche");
+                Console.WriteLine("2.Frenar coche");
+                Console.WriteLine("3.Mostrar informacion");
+
+                opcion = int.Parse(Console.ReadLine());
+                switch (opcion)
+                {
+                    case 1:
+                        Console.WriteLine("Cual es la cantidad a la que va a incrementar  la velocidad del coche?");
+                        cantidad = int.Parse(Console.ReadLine());
+
+                        if (cantidad > 0)
+                        {
+                            
+                            Console.WriteLine("La velociada actual del coche es de: " + coche.Acelerar(cantidad)+"km/h");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Al acelerar no se pueden poner numeros negativos");
+                        }
+                        
+                        break;
+
+                    case 2:
+                        Console.WriteLine("Cual es la cantidad a la que va a decrementar la velocidad del coche?");
+                        cantidad = int.Parse(Console.ReadLine());
+
+                        
+                            Console.WriteLine("La velocidad actual es: " + coche.Frenar(cantidad)+"km/h");
+                        
+                        
+                        break;
+                    case 3:
+
+                        Console.WriteLine(coche.MostrarInfo());
+                        break;
+                    default:
+                        Console.WriteLine("Adios pues");
+                        break;
+                }
+                cantidad = 0;
+            } while (opcion<4);
+        }
+    }
+}
